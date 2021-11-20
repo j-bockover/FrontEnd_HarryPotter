2b3caabd60007eeb22875bf9e72524decae1c1cf
Jason <jasb2@pdx.edu>
2021-11-19 21:35:30 -0800

    Fixed quiz and added spells page

--- harry-potter-app/src/index.js
+++ harry-potter-app/src/index.js
@@ -4,17 +4,19 @@ import './index.css';
 import App from './App';
 
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Home from "./routes/Home"
-import Search from "./routes/Search"
+import Home from "./routes/Home.js"
+import Search from "./routes/Search.js"
+import Spells from "./routes/Spells.js"
 
 const rootElement = document.getElementById('root')
 
 ReactDOM.render(
   <BrowserRouter>
     <Routes>
-      <Route path='/' element={<App />} />
-      <Route path='home' element={<Home />} />
-      <Route path='search' element={<Search />} />
+      <Route path="/" element={<App />} />
+      <Route path="home" element={<Home />} />
+      <Route path="search" element={<Search />} />
+      <Route path="spells" element={<Spells />} />
     </Routes>
   </BrowserRouter>,
   rootElement
---
+++ harry-potter-app/src/routes/Spells.js
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../App.css";
+import Navbar from "../components/navbar.js";
+
+
+function Spells() {  
+
+  return (
+    <div className = "Spells">
+      <Navbar />
+    </div>
+  );
+}
+
+export default Spells;
