ed3923307f198a9e80356bf91f78691771bceb55
Jason <jasb2@pdx.edu>
2021-11-21 17:05:36 -0800

    updated spells.js and spellsearch.js

---
+++ harry-potter-app/src/components/SpellSearch.js
@@ -0,0 +1,29 @@
+import { useState, useEffect, useCallback } from "react";
+import Spells from '../routes/Spells.js'
+
+function SpellSearch() {
+
+const url = `https://fedeperin-harry-potter-api-en.herokuapp.com/spells/`;
+let [spellnames, setSpellnames] = useState([]);
+let [isLoaded, setIsLoaded] = useState(false);
+
+useEffect(() => {
+  fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        //next use setCharacters function and set
+        //it to the results array
+        setSpellnames(data);
+        setIsLoaded(true);
+      })
+      .catch((error) => console.log(error));
+  }, [url]); //can put url here but it never changes
+
+  
+  const spellData = spellnames;
+  return spellData;
+
+}
+
+export default SpellSearch;
--- harry-potter-app/src/routes/Spells.js
+++ harry-potter-app/src/routes/Spells.js
@@ -1,20 +1,77 @@
-import React, { useEffect, useState } from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "../App.css";
-import Navbar from "../components/navbar.js";
+import React, { useState, useEffect } from "react";
+import Navbar from "../components/navbar";
+import SpellSearch from "../components/SpellSearch";
+
+const spellsimg = `https://static.wikia.nocookie.net/harrypotter/images/0/0e/Alohomora.gif/revision/latest/scale-to-width-down/490?cb=20140811055322`;
+
+function Spells(props) {
+  const spellData = SpellSearch();
+  //console.log(spellData[1]); //woohoo!!! now
 
 
-function Spells() {
+  const [query, setQuery] = useState("");
+  const [spell, setSpell] = useState(null); //get spell
+  const [use, setUse] = useState(null); //get spell use
+
+  async function getSpell(props) {
+    console.log(props);
+    let url = `https://fedeperin-harry-potter-api-en.herokuapp.com/spells/${query}`;
+    console.log(url);
+
+    console.log("query", query); // this query will be attached to url
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+      console.log(data.spell, data.use); //get the spell name and the use of the spell
+      setSpell(data.spell);
+      setUse(data.use);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    getSpell();
+  }, [query]);
+
+  function handleChange(event) {
+    event.preventDefault();
+    for (let i = 0; i < 72; i++) {
+      if (event.target.value === spellData[i].spell) {
+        console.log(spellData[i].id);
+        setQuery(spellData[i].id);
+      }
+    }
+  }
+
 
   return (
-    <div className="Spells">
+    <div>
       <Navbar />
-      <body className="spellsbody">
-        <h1 className="spellsheader"> Spells</h1>
-      </body>
+      <h1 className="spellsheader">Spells</h1>
+      <div className="container">
+        <form method="get">
+          <label className="searchintro" htmlFor="userInput">
+            Search for a spell!
+          </label>
+          <br />
+          <input type="text" id="userInput" onChange={handleChange} />
+          <input type="reset" value="Reset" />
+        </form>
+        <div className="searchspell searchresults text-center">{spell}</div>
+        <div className="searchresults text-center">{use}</div>
+
+        <div className="listheader text-center">List of Spells</div>
+        <ul className="list text-center">
+          {" "}
+          {spellData.map((item) => {
+            return <li>{item.spell}</li>;
+          })}
+        </ul>
+      </div>
     </div>
   );
 }
 
 export default Spells;
-
